4f44396f1f95217a378cc71024a3e97f34379b6b
Grant Ferowich <g.ferowich@gmail.com>
2019-10-25 21:04:53 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,29 @@
-# My Code here....
+map_to_negativize(source_array)
+  new_arr = []
+  i = 0 
+  
+  while i < source_array.length 
+  ele = source_array[i]
+  new_arr.push(ele*-1)
+  i += 1
+  end
+
+  new_arr
+end
+
+map_to_no_change(source_array)
+  new_arr = []
+  i = 0 
+  
+  while i < source_array.length 
+  ele = source_array[i]
+  new_arr.push(ele)
+  i += 1
+  end
+
+  new_arr
+end
+ 
+
+map_to_double(source_array)
+map_to_square(source_array)# My Code here....
