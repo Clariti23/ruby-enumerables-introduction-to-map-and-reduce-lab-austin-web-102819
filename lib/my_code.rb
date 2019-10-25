eaf97057872dbab00efbc209b09feb2bf55c262c
Grant Ferowich <g.ferowich@gmail.com>
2019-10-25 21:07:26 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -52,8 +52,14 @@ def map_to_square(source_array)
   new_arr
 end
 
-
-
+def reduce_to_total(source_array, starting_point)
+  total = 0 
+  
+  
+  
+end  
+- `reduce_to_all_true(source_array)`
+- `reduce_to_any_true(source_array)`
 
 
 # My Code here....
